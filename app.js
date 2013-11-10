71381bc8894000dd3e733b5ea39150fab4da5abc
jybox <m@jybox.net>
2013-11-10 11:37:28 +0800

    front-end of signup

--- app.js
+++ app.js
@@ -7,19 +7,13 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var i18next = require('i18next');
-var routes = require('./routes');
-var user = require('./routes/user');
+
 
 var app = express();
 
-i18next.init({
-    lng: 'zh_CN',
-    saveMissing: true,
-    debug: true
-});
+i18next.init();
 i18next.registerAppHelper(app);
 
-// all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -32,13 +26,16 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'static')));
 
-// development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+(function() {
+  var view = require('./routes/view');
+
+  app.get('/', view.index);
+  app.get('/account/signup/', view.signup);
+})();
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ routes/view.js
@@ -0,0 +1,7 @@
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.signup = function(req, res){
+  res.render('signup', { title: 'Express' });
+};
