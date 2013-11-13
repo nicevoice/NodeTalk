ea9e65370e63c2584cbc0480038e42389bbb305d
jybox <m@jybox.net>
2013-11-13 10:41:00 +0800

    create test

---
+++ test/account-test.js
@@ -0,0 +1,17 @@
+var request = require('request');
+var vows = require('vows');
+var assert = require('assert');
+
+vows.describe('/account/').addBatch({
+  "/account/singup/": {
+    'topic': function () {
+      request({
+        uri: 'http://127.0.0.1:3000/account/signup/',
+        method: 'GET'
+      }, this.callback)
+    },
+    "should respond with 200": function (err, res, body) {
+      assert.equal(res.statusCode, 200);
+    }
+  }
+}).export(module);
