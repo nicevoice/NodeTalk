0aa49a281bff580200c84400aecd4b6213f8c303
jysperm <m@jybox.net>
2013-12-18 09:24:52 +0800

    format

--- utils.js
+++ utils.js
@@ -1,23 +1,26 @@
 var crypto = require('crypto');
 var auth = require('./auth');
 
-exports.sha256 = function(data) {
-  return crypto.createHash('sha256').update(data).digest('hex');
+module.exports = {
+    sha256: function(data) {
+        return crypto.createHash('sha256').update(data).digest('hex');
+    },
+
+    md5: function(data) {
+        return crypto.createHash('md5').update(data).digest('hex');
+    },
+
+    timestamp: function() {
+        return Math.round(Date.now() / 1000);
+    },
+
+    render: function(req, res, name, param) {
+        auth.authenticate(req, function(err, account) {
+            if(!param)
+                param = {};
+            param.account = err ? undefined : account;
+            res.render(name, param);
+        });
+    }
 };
 
-exports.md5 = function(data) {
-  return crypto.createHash('md5').update(data).digest('hex');
-};
-
-exports.timestamp = function() {
- return Math.round(Date.now() / 1000);
-};
-
-exports.render = function(req, res, name, param) {
-  auth.authenticate(req, function(err, account) {
-    if(!param)
-      param = {};
-    param.account = err ? undefined : account;
-    res.render(name, param);
-  });
-};
