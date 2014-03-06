e5b72e197198f2a9dd2890cea927c91486f006f1
oyiadin <oyiadin@gmail.com>
2014-03-06 20:47:29 +0800

    create node

--- handlers/node.js
+++ handlers/node.js
@@ -9,6 +9,41 @@ module.exports = {
           utils.render(req, res, 'nodeIndex', {nodes: result});
         });
       });
+    },
+    'create': function(req, res) {
+      utils.authenticate(req, function(err, account) {
+        if (err)
+          res.redirect('/login');
+
+        utils.render(req, res, 'nodeCreate');
+      });
+    }
+  },
+
+  post: {
+    'create': function(req, res) {
+      if (!req.body['nodename'])
+        utils.render(
+          req, res, 'nodeCreate', {errorMsg: req.t('handler.missingValues')});
+
+      db.nodes.find({name: req.body['nodename']}, function(err, cursor) {
+        cursor.toArray(function(err, result) {
+          console.log(result);
+          if (!result.length == 0) {
+            utils.render(
+              req, res, 'nodeCreate', {errorMsg: req.t('handler.nodeExisted')});
+          } else {
+            db.nodes.insert({
+              name: req.body['nodename'],
+              title: req.body['nodetitle'] || req.body['nodename'],
+              description: req.body['nodedescription'] || ''
+            }, function(err) {
+              if (!err)
+                res.redirect('/node/');
+            });
+          }
+        });
+      });
     }
   }
 };
